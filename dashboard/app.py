45ca66efc5d99ce03c02e931fbb65a59732434c3
ParkYourCode <andrewcoderer@gmail.com>
2026-07-03 21:02:06 -0700

    Add initial Airflow test and Streamlit dashboard

---
+++ dashboard/app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Flight Redemption Platform",
+    page_icon="✈️",
+    layout="wide"
+)
+
+st.title("✈️ Flight Redemption Platform")
+
+st.write("Coming Soon...")
